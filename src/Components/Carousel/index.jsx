a502ee5cc51407ffe61fa91c17fa91e746e85335
Priscille-LR <76170675+Priscille-LR@users.noreply.github.com>
2021-10-28 12:24:03 +0200

    update carousel

--- src/Components/Carousel/index.jsx
+++ src/Components/Carousel/index.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import '../Carousel/Carousel.css';
+import chevronLeft from '../../Assets/chevron-left.png';
+import chevronRight from '../../Assets/chevron-right.png';
 
 class Carousel extends Component {
    constructor(props) {
@@ -11,7 +13,13 @@ class Carousel extends Component {
       const { picture } = this.props;
       return (
          <div className="carousel-wrapper">
-            <img src={picture} alt="rental" className="carousel-item" />
+            <button>
+               <img src={chevronLeft} alt="" className="button-previous" />
+            </button>
+            <img src={picture} alt="rental" className="carousel__item" />
+            <button>
+               <img src={chevronRight} alt="" className="button-next" />
+            </button>
          </div>
       );
    }
